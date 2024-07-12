9445fe6a76f00dc35c9be962982877b8f5fe1850
kruleshvpn <34224735+kruleshvpn@users.noreply.github.com>
2024-07-12 14:24:38 +0800

    Update start.go

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -26,6 +26,6 @@ func start(ctx *ext.Context, u *ext.Update) error {
 		ctx.Reply(u, "You are not allowed to use this bot.", nil)
 		return dispatcher.EndGroups
 	}
-	ctx.Reply(u, "WELCOME " . chatId ."\n-------------------\nPadooTV will serve you a direct link once you upload media file\n-------------------\nNote!!\n\nAny media content should be respect the rules and regulation!\n\nNo pornographic content, will lead to permanent banned if pursue\n-------------------\nThanks for supporting us.", nil)
+	ctx.Reply(u, "WELCOME TO PADOOTV\n-------------------\nPadooTV will serve you a direct link once you upload media file\n-------------------\nNote!!\n\nAny media content should be respect the rules and regulation!\n\nNo pornographic content, will lead to permanent banned if pursue\n-------------------\nThanks for supporting us.", nil)
 	return dispatcher.EndGroups
 }
